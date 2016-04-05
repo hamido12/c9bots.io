9361c088adc85e018004d26410d671244ea44379
problemsofworld <amenahmed1200@gmail.com>
2016-04-05 05:03:41 +0300

    IDK
    1-clone to c9.io
    2-change uuid and make sure to put urs
    3-make sure to change ('ws//:example1-user3.c9users.io:8081') in ur scripte
    
    4- run server.js make sure it gonna connect with ur uuid and ip 
    5-run F.js and the bots will come

---
+++ a/names.js
@@ -0,0 +1,12 @@
+﻿var names = {};
+
+names.nameList = [
+"♥lag maker",
+"#stopbot",
+];
+
+names.getRandomName = function() {
+    return names.nameList[Math.floor((Math.random() * names.nameList.length))];
+};
+
+module.exports = names;
